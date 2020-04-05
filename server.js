18c9acad2fe5781e6ab2fcc0cb4d45030202d3cb
patrick dorgu <pat.dorgu@gmail.com>
2020-04-05 05:49:42 -0400

    updates

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 
 const app = express();
-
+const path = require("path");
+const fs = require("fs");
+const db = require("./db/db.json")
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '/public'));
 
 
 // Routes
