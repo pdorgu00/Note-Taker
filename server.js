502b735e8ca3b5823420beb8429ff4555deb182d
patrick dorgu <pat.dorgu@gmail.com>
2020-04-05 05:40:31 -0400

    updated package json

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + './public'));
 
 
 // Routes
